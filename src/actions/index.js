89f38ae56f77cb970ec52703462266c0dde5125d
pjliddy <pjliddy@gmail.com>
2019-05-03 12:56:43 -0400

    Memoized getUser with lodash

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 // Action creator that returns a function
@@ -6,7 +7,12 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => {
+  _fetchUser(id, dispatch);
+};
+
+// private function
+const _fetchUser = _.memoize(async(id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: 'FETCH_USER', payload: response.data });
-};
+});
