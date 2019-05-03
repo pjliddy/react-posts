1a8009da0a47ececcedd3b62a5bf76fbf8e4c02a
pjliddy <pjliddy@gmail.com>
2019-05-03 12:10:17 -0400

    Added action and reducer to fetch users

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,3 +5,8 @@ export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get('/posts');
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
